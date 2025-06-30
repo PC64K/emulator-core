3526f87b14ebbad00d70353073b28b95a01a54ed
Milk-Cool <coder.tomsk@gmail.com>
2025-06-30 19:28:12 +0300

    basic initialization stuff + callbacks

---
+++ include/pc64k.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Returns disk size
+typedef size_t(*PC64KDiskSizeGetter)();
+// Allocates memory for read data and reads data. Doesn't free memory.
+typedef uint8_t*(*PC64KDiskReader)(size_t pos, uint8_t len);
+// Writes data to disk.
+typedef void(*PC64KDiskWriter)(size_t pos, uint8_t* data, uint8_t len);
+
+// Gets microseconds since an arbitrary point in time
+typedef uint64_t(*PC64KMicrosGetter)();
+
+typedef struct {
+    uint16_t pc;
+    uint8_t reg[16];
+    uint8_t ram[65536];
+    uint8_t display[320][192];
+    uint8_t stack_pos;
+    uint16_t stack[128];
+
+    PC64KDiskSizeGetter cb_disk_size;
+    PC64KDiskReader cb_disk_read;
+    PC64KDiskWriter cb_disk_write;
+    PC64KMicrosGetter cb_micros;
+} PC64K;
+
+PC64K* pc64k_alloc_init(uint8_t* rom, size_t rom_size,
+    PC64KDiskSizeGetter cb_disk_size,
+    PC64KDiskReader cb_disk_read,
+    PC64KDiskWriter cb_disk_write,
+    PC64KMicrosGetter cb_micros);
+void pc64k_deinit_free(PC64K* ctx);
---
+++ src/pc64k.c
@@ -0,0 +1,28 @@
+#include <pc64k.h>
+
+PC64K* pc64k_alloc_init(uint8_t* rom, size_t rom_size,
+    PC64KDiskSizeGetter cb_disk_size,
+    PC64KDiskReader cb_disk_read,
+    PC64KDiskWriter cb_disk_write,
+    PC64KMicrosGetter cb_micros) {
+    if(rom_size > 65535)
+        return NULL;
+    PC64K* ctx = (PC64K*) malloc(sizeof(PC64K));
+    memcpy(ctx->ram, rom, rom_size);
+
+    ctx->cb_disk_size = cb_disk_size;
+    ctx->cb_disk_read = cb_disk_read;
+    ctx->cb_disk_write = cb_disk_write;
+    ctx->cb_micros = cb_micros;
+
+    ctx->stack_pos = 0;
+    ctx->pc = 0;
+
+    memset(ctx->display, 0, sizeof(ctx->display));
+    memset(ctx->reg, 0, sizeof(ctx->reg));
+
+    return ctx;
+}
+void pc64k_deinit_free(PC64K* ctx) {
+    free(ctx);
+}
